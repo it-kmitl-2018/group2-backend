099e63ff6c14b675864e46818c40ef023fcdc4d8
Chayanon Thongpila <58070021@kmitl.ac.th>
2018-03-05 21:34:58 +0700

    Refactor and delete unused package.

--- src/main/java/th/ac/kmitl/soa/group2/model/TypeCodeTest.java
+++ src/main/java/th/ac/kmitl/soa/group2/model/TypeCodeTest.java
@@ -1,4 +1,9 @@
 package th.ac.kmitl.soa.group2.model;
 
 public class TypeCodeTest {
+    public static void main(String[] args){
+        for(TypeCode tc : TypeCode.values()){
+            System.out.println(tc.getTypeCode()+ " " +tc.getDescription());
+        }
+    }
 }

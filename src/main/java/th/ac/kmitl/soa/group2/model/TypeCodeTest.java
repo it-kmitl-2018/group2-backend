9d275769aae52f858149bf9ca2f3a41dd5a5f2b0
kavinvin <kavinvin.vin@gmail.com>
2018-03-06 01:33:28 +0700

    Add TypeCodeTest

--- src/main/java/th/ac/kmitl/soa/group2/model/TypeCodeTest.java
+++
@@ -1,9 +0,0 @@
-package th.ac.kmitl.soa.group2.model;
-
-public class TypeCodeTest {
-    public static void main(String[] args){
-        for(TypeCode tc : TypeCode.values()){
-            System.out.println(tc.getTypeCode()+ " " +tc.getDescription());
-        }
-    }
-}

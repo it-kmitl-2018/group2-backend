d8bebfd19effa3b67e0a06f1183556f6556858b5
nutchy <58070021@kmitl.ac.th>
2018-03-13 04:05:07 +0700

    Add ExchangedDocumentWrapper and unit test

--- src/main/java/th/ac/kmitl/soa/group2/models/ExchangedDocumentWrapper.java
+++ src/main/java/th/ac/kmitl/soa/group2/models/ExchangedDocumentWrapper.java
@@ -1,22 +1,55 @@
 package th.ac.kmitl.soa.group2.models;
 
-import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
-import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
-import lombok.Value;
-import static th.ac.kmitl.soa.group2.definitions.XmlTags.CONTENT;
-import static th.ac.kmitl.soa.group2.definitions.XmlTags.INCLUDED_NOTE;
-import static th.ac.kmitl.soa.group2.definitions.XmlTags.SUBJECT;
+import io.vavr.control.Option;
+import th.ac.kmitl.soa.group2.definitions.DocumentType;
+
+import java.sql.Timestamp;
 
 public class ExchangedDocumentWrapper {
 
-    @Value
-    @JacksonXmlRootElement(localName = INCLUDED_NOTE)
-    public static class Note {
+    // TODO : Replace subject and content by TaxInvoiceForm model.
+    private String subject, content, id, name, typeCode, purpose, purposeCode, globalid;
+    private Timestamp createAt, issueAt;
 
-        @JacksonXmlProperty(localName = SUBJECT)
-        public String subject;
+    public ExchangedDocumentWrapper(String subject,
+                                    String content,
+                                    String id,
+                                    String name,
+                                    String typeCode,
+                                    String purpose,
+                                    String purposeCode,
+                                    String globalid,
+                                    Timestamp issueAt,
+                                    Timestamp createAt) {
+        // TODO : need to replace all args by TaxInvoiceForm.
+        this.subject = subject;
+        this.content = content;
+        this.id = id;
+        this.name = name;
+        this.typeCode = typeCode;
+        this.purpose = purpose;
+        this.purposeCode = purposeCode;
+        this.globalid = globalid;
+        this.createAt = createAt;
+        this.issueAt = issueAt;
+    }
 
-        @JacksonXmlProperty(localName = CONTENT)
-        public String content;
+    public HeaderModel getModel(){
+        HeaderNoteModel headerNoteModel = new HeaderNoteModel(subject, content);
+
+        // TODO : All fields will replace by TaxInvoiceForm fields.
+        HeaderModel headerModel = new HeaderModel(
+                id,
+                name,
+                typeCode,
+                issueAt,
+                Option.some(purpose),
+                Option.some(purposeCode),
+                Option.some(globalid),
+                createAt,
+                headerNoteModel
+        );
+
+        return headerModel;
     }
 }

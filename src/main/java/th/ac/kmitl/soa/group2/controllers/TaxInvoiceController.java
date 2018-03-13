c5ffa1f4d1cc8a1fe66410703135b8341c906a61
kavinvin <kavinvin.vin@gmail.com>
2018-03-13 11:38:26 +0700

    Remove XML serialization

--- src/main/java/th/ac/kmitl/soa/group2/controllers/TaxInvoiceController.java
+++ src/main/java/th/ac/kmitl/soa/group2/controllers/TaxInvoiceController.java
@@ -5,17 +5,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import th.ac.kmitl.soa.group2.forms.TaxInvoiceForm;
 import th.ac.kmitl.soa.group2.messages.SuccessMessage;
+import th.ac.kmitl.soa.group2.models.TaxInvoiceModel;
 
 import javax.validation.Valid;
-
-import static th.ac.kmitl.soa.group2.utils.binders.Xml.serialize;
 
 @RestController
 public class TaxInvoiceController {
 
     @PostMapping(value = "/tax-invoice/generate")
-    public SuccessMessage<String> generate(@Valid @RequestBody TaxInvoiceForm taxInvoiceForm) {
-        return serialize(taxInvoiceForm.toModel()).map(SuccessMessage::new).get();
+    public SuccessMessage<TaxInvoiceModel> generate(@Valid @RequestBody TaxInvoiceForm taxInvoiceForm) {
+        return new SuccessMessage<>(taxInvoiceForm.toModel());
     }
 
 }

e11dc60bf5c1a657087e246ed70350b79e6ae88c
nutchy <58070021@kmitl.ac.th>
2018-03-16 15:09:00 +0700

    Add the EmailUriUniversalCommunicationModel.

---
+++ src/main/java/th/ac/kmitl/soa/group2/models/supplychaintradetransaction/EmailUriUniversalCommunicationModel.java
@@ -0,0 +1,14 @@
+package th.ac.kmitl.soa.group2.models.supplychaintradetransaction;
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import lombok.Value;
+
+import static th.ac.kmitl.soa.group2.definitions.EtdaXmlTags.URI_ID;
+
+@Value
+public class EmailUriUniversalCommunicationModel {
+
+    @JacksonXmlProperty(localName = URI_ID)
+    String uriId;
+
+}

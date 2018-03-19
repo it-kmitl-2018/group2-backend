bafdb8d62f202940bde1a405fed67d03e57d4b4d
nutchy <58070021@kmitl.ac.th>
2018-03-19 13:34:26 +0700

    Test model by serialize without root element.

--- src/test/java/th/ac/kmitl/soa/group2/models/supplychaintradetransaction/EmailUriModelTest.java
+++ src/test/java/th/ac/kmitl/soa/group2/models/supplychaintradetransaction/EmailUriModelTest.java
@@ -1,22 +1,22 @@
 package th.ac.kmitl.soa.group2.models.supplychaintradetransaction;
 
 import org.junit.jupiter.api.Test;
-import th.ac.kmitl.soa.group2.utils.binders.Xml;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static th.ac.kmitl.soa.group2.definitions.EtdaXmlTags.URI_ID;
+import static th.ac.kmitl.soa.group2.utils.binders.Xml.serializeWithoutRoot;
+import static th.ac.kmitl.soa.group2.utils.binders.Xml.tag;
 
 public class EmailUriModelTest {
 
     private static final String email = "abc@xyz.com";
+
     private static final EmailUriModel model = new EmailUriModel(email);
 
-    private final String xml =
-        "<ram:EmailURIUniversalCommunication>"+
-            "<ram:URIID>"+ email + "</ram:URIID>" +
-        "</ram:EmailURIUniversalCommunication>";
+    private final String xml = tag(URI_ID, email);
 
     @Test
     public void shouldCreateCorrectXml(){
-        assertEquals(Xml.serialize(model).get(), xml);
+        assertEquals(serializeWithoutRoot(model).get(), xml);
     }
 }
--- src/test/java/th/ac/kmitl/soa/group2/models/supplychaintradetransaction/PhoneNumberModelTest.java
+++ src/test/java/th/ac/kmitl/soa/group2/models/supplychaintradetransaction/PhoneNumberModelTest.java
@@ -1,21 +1,22 @@
 package th.ac.kmitl.soa.group2.models.supplychaintradetransaction;
 
 import org.junit.jupiter.api.Test;
-import th.ac.kmitl.soa.group2.utils.binders.Xml;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static th.ac.kmitl.soa.group2.definitions.EtdaXmlTags.COMPLETE_NUMBER;
+import static th.ac.kmitl.soa.group2.utils.binders.Xml.serializeWithoutRoot;
+import static th.ac.kmitl.soa.group2.utils.binders.Xml.tag;
 
 public class PhoneNumberModelTest {
 
     private final String phoneNumber = "0812345678";
-    private final String xml =
-        "<ram:TelephoneUniversalCommunication>" +
-            "<ram:CompleteNumber>" + phoneNumber + "</ram:CompleteNumber>" +
-        "</ram:TelephoneUniversalCommunication>";
+
+    private final PhoneNumberModel model = new PhoneNumberModel(phoneNumber);
+
+    private final String xml = tag(COMPLETE_NUMBER, phoneNumber);
 
     @Test
     public void shoudBeCorrectPhoneNumber() {
-        PhoneNumberModel phoneNumberModel = new PhoneNumberModel(phoneNumber);
-        assertEquals(Xml.serialize(phoneNumberModel).get(), xml);
+        assertEquals(serializeWithoutRoot(model).get(), xml);
     }
 }

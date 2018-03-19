10494e1e7fc6178fbbd1d026e7f2843d34f5f9d3
nutchy <58070021@kmitl.ac.th>
2018-03-19 13:33:29 +0700

    Delete xml root element.

--- src/main/java/th/ac/kmitl/soa/group2/models/supplychaintradetransaction/EmailUriModel.java
+++ src/main/java/th/ac/kmitl/soa/group2/models/supplychaintradetransaction/EmailUriModel.java
@@ -1,14 +1,11 @@
 package th.ac.kmitl.soa.group2.models.supplychaintradetransaction;
 
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
-import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 import lombok.Value;
 
-import static th.ac.kmitl.soa.group2.definitions.EtdaXmlTags.EMAIL_URI_UNIVERSAL_COMMUNICATION;
 import static th.ac.kmitl.soa.group2.definitions.EtdaXmlTags.URI_ID;
 
 @Value
-@JacksonXmlRootElement(localName = EMAIL_URI_UNIVERSAL_COMMUNICATION)
 public class EmailUriModel {
 
     @JacksonXmlProperty(localName = URI_ID)
--- src/main/java/th/ac/kmitl/soa/group2/models/supplychaintradetransaction/PhoneNumberModel.java
+++ src/main/java/th/ac/kmitl/soa/group2/models/supplychaintradetransaction/PhoneNumberModel.java
@@ -1,14 +1,11 @@
 package th.ac.kmitl.soa.group2.models.supplychaintradetransaction;
 
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
-import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 import lombok.Value;
 
 import static th.ac.kmitl.soa.group2.definitions.EtdaXmlTags.COMPLETE_NUMBER;
-import static th.ac.kmitl.soa.group2.definitions.EtdaXmlTags.TELEPHONE_UNIVERSAL_COMMUNICATION;
 
 @Value
-@JacksonXmlRootElement(localName = TELEPHONE_UNIVERSAL_COMMUNICATION)
 public class PhoneNumberModel {
 
     @JacksonXmlProperty(localName = COMPLETE_NUMBER)

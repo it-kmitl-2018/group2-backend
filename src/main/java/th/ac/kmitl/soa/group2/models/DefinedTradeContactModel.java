b62e1007281608ca9966d09361e3c58179aa7641
J <58070014@kmitl.ac.th>
2018-03-20 12:43:37 +0700

    Fix DefinedTradeContactModelTest

--- src/main/java/th/ac/kmitl/soa/group2/models/DefinedTradeContactModel.java
+++ src/main/java/th/ac/kmitl/soa/group2/models/DefinedTradeContactModel.java
@@ -1,6 +1,7 @@
 package th.ac.kmitl.soa.group2.models;
 
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import io.vavr.control.Option;
 import lombok.Value;
 
 import static th.ac.kmitl.soa.group2.definitions.EtdaXmlTags.DEPARTMENT_NAME;
@@ -10,9 +11,9 @@ import static th.ac.kmitl.soa.group2.definitions.EtdaXmlTags.PERSON_NAME;
 class DefinedTradeContactModel {
 
     @JacksonXmlProperty(localName = PERSON_NAME)
-    String personName;
+    Option<String> personName;
 
     @JacksonXmlProperty(localName = DEPARTMENT_NAME)
-    String departmentName;
+    Option<String> departmentName;
 
 }
--- src/test/java/th/ac/kmitl/soa/group2/models/DefinedTradeContactModelTest.java
+++ src/test/java/th/ac/kmitl/soa/group2/models/DefinedTradeContactModelTest.java
@@ -1,27 +1,29 @@
 package th.ac.kmitl.soa.group2.models;
 
+import io.vavr.control.Option;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static th.ac.kmitl.soa.group2.utils.binders.Xml.serialize;
+import static th.ac.kmitl.soa.group2.definitions.EtdaXmlTags.DEPARTMENT_NAME;
+import static th.ac.kmitl.soa.group2.definitions.EtdaXmlTags.PERSON_NAME;
+import static th.ac.kmitl.soa.group2.utils.binders.Xml.serializeWithoutRoot;
+import static th.ac.kmitl.soa.group2.utils.binders.Xml.tag;
 
 public class DefinedTradeContactModelTest {
 
     private static final DefinedTradeContactModel definedTradeContact =
         new DefinedTradeContactModel(
-            "ธารา ยืนขำ",
-            "marketing"
+            Option.some("ธารา ยืนขำ"),
+            Option.some("marketing")
         );
 
     private final String xml =
-        "<DefinedTradeContactModel>" +
-            "<ram:PersonName>ธารา ยืนขำ</ram:PersonName>" +
-            "<ram:DepartmentName>marketing</ram:DepartmentName>" +
-        "</DefinedTradeContactModel>";
+        tag(PERSON_NAME, "ธารา ยืนขำ")
+        + tag(DEPARTMENT_NAME, "marketing");
 
     @Test
     public void shouldCreateCorrectXml() {
-        assertEquals(serialize(definedTradeContact).get(), xml);
+        assertEquals(serializeWithoutRoot(definedTradeContact).get(), xml);
     }
 
 }

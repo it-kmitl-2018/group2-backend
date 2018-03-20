98686122f9b44b397c2b9b19deca015a52cc8b94
J <58070014@kmitl.ac.th>
2018-03-20 12:28:32 +0700

    Fix ApplicableTradeTaxModelTest

--- src/main/java/th/ac/kmitl/soa/group2/models/ApplicableTradeTaxModel.java
+++ src/main/java/th/ac/kmitl/soa/group2/models/ApplicableTradeTaxModel.java
@@ -1,6 +1,7 @@
 package th.ac.kmitl.soa.group2.models;
 
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import io.vavr.control.Option;
 import lombok.Value;
 
 import static th.ac.kmitl.soa.group2.definitions.EtdaXmlTags.*;
@@ -15,9 +16,9 @@ class ApplicableTradeTaxModel {
     int calculatedRate;
 
     @JacksonXmlProperty(localName = BASIS_AMOUNT)
-    float basisAmount;
+    Option<Float> basisAmount;
 
     @JacksonXmlProperty(localName = CALCULATE_AMOUNT)
-    float calculatedAmount;
+    Option<Float> calculatedAmount;
 
 }

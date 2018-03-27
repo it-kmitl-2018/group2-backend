fc92cf285a71b618d26466e38b46628ffae48e7f
Kawin Smathi <58070006@kmitl.ac.th>
2018-03-27 11:01:11 +0700

    create the SpecifiedTradePaymentTermsTest.

---
+++ src/test/java/th/ac/kmitl/soa/group2/models/SpecifiedTradePaymentTermsTest.java
@@ -0,0 +1,30 @@
+package th.ac.kmitl.soa.group2.models;
+
+import io.vavr.control.Option;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static th.ac.kmitl.soa.group2.definitions.EtdaXmlTags.*;
+import static th.ac.kmitl.soa.group2.utils.binders.Xml.serializeWithoutRoot;
+import static th.ac.kmitl.soa.group2.utils.binders.Xml.tag;
+
+public class SpecifiedTradePaymentTermsTest {
+
+    private static final SpecifiedTradePaymentTermsModel
+            SpecifiedTradePaymentTerms =
+            new SpecifiedTradePaymentTermsModel(
+                    "1101",
+                    Option.of("very good"),
+                    "10-10-2016"
+            );
+
+    private final String xml =
+            tag(TYPE_CODE, "1101")
+                    + tag(DESCRIPTION, "very good")
+                    + tag(DUE_DATE_TIME, "10-10-2016");
+
+    @Test
+    public void shouldCreateCorrectXml() {
+        assertEquals(serializeWithoutRoot(SpecifiedTradePaymentTerms).get(), xml);
+    }
+}

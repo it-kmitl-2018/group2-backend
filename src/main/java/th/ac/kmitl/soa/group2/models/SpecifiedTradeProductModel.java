4b3a64f0a258450b17d1ba0b99b2bf7c8b241a97
J <saii16320@gmail.com>
2018-05-14 22:51:21 +0700

    Add the SpecifiedTradeProductModel.

---
+++ src/main/java/th/ac/kmitl/soa/group2/models/SpecifiedTradeProductModel.java
@@ -0,0 +1,24 @@
+package th.ac.kmitl.soa.group2.models;
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import io.vavr.control.Option;
+import lombok.Value;
+
+import static th.ac.kmitl.soa.group2.definitions.EtdaXmlTags.*;
+
+@Value
+public class SpecifiedTradeProductModel {
+
+        @JacksonXmlProperty(localName = ID)
+        String id;
+
+        @JacksonXmlProperty(localName = GLOBAL_ID)
+        Option<String> globalId;
+
+        @JacksonXmlProperty(localName = NAME)
+        String name;
+
+        @JacksonXmlProperty(localName = DESCRIPTION)
+        Option<String> description;
+
+}
